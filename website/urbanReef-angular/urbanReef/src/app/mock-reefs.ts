c72cd36787f4cdb7b3735fc6fd66cabc2eb77fc5
FrostyDog132 <nielsnoordzij2001@gmail.com>
2023-10-18 14:05:18 +0200

    Added mock file

---
+++ website/urbanReef-angular/urbanReef/src/app/mock-reefs.ts
@@ -0,0 +1,7 @@
+import {reef} from "./models/reef.model";
+
+export const REEFS: reef[] = [
+  new reef(1, "Reef 1A", ["http://animalsbreeds.com/wp-content/uploads/2014/11/Shiba-Inu-4.jpg", "source"], "Green house", 55, 21),
+  new reef(2,"Reef 1B",[] ,"Green house", 60, 22),
+  new reef(3, "Reef 1C", ["https://www.rover.com/blog/wp-content/uploads/2019/07/shiba-inu-3.jpg", "source"], "Green house", 55, 21),
+]
